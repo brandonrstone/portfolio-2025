0144753520e9b909c2651ef3d92f69343836a084
Brandon Stone <42560473+KidLiberty@users.noreply.github.com>
2025-04-22 10:56:55 -0400

    Not found page fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,11 @@ export default function NotFound() {
   return (
     <div className='min-h-screen flex flex-col justify-center items-center text-center p-6'>
       <Image src='/images/Not-Found.png' alt='Confused developer... how did you get here?' width={300} height={300} />
-      <h1 className='text-4xl font-bold mb-4'><span className='font-extrabold'>404</span> - Page Not Found</h1>
+      <h1 className='flex flex-col sm:flex-row mb-4 text-4xl font-bold'>
+        <div className='font-extrabold'>404</div>
+        <div className='hidden sm:block px-2'>-</div>
+        <div>Page Not Found</div>
+      </h1>
       <p className='mb-6 text-gray-500'>{`What you are looking for is not here, or perhaps it has been moved.`}</p>
       <Link href='/' className='max-w-3xl py-3 px-4 rounded-lg bg-gradient-to-r from-pink-400 to-red-500 hover:from-pink-500 hover:to-red-600 text-foreground font-semibold focus:ring-2 focus:ring-pink-300 cursor-pointer transition-all duration-700 ease-in-out' aria-label='Navigate back to home page'>
         Take me home!
